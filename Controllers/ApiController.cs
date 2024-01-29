19567c9b9b440476a3d5ab38a98de8da412df472
HsiaoHungWang <wang@ispan.com.tw>
2024-01-29 16:41:00 +0800

    建立 EF

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MSIT155Site.Models;
+
+namespace MSIT155Site.Controllers
+{
+    public class ApiController : Controller
+    {
+        private readonly MyDBContext _context;
+        public ApiController(MyDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Cities() {           
+           var cities = _context.Addresses.Select(a => a.City).Distinct();
+            return Json(cities);
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using MSIT155Site.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<MyDBContext>(
+    options => options.UseSqlServer(
+        builder.Configuration.GetConnectionString("MyDBConnection")
+));
+
+
 
 var app = builder.Build();
 
