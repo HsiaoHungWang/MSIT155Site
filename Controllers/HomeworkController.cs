9ab1f3b5a8effeb81015fd840f34e32493fbcaf0
HsiaoHungWang <wang@ispan.com.tw>
2024-01-29 16:13:38 +0800

    作業

---
+++ Controllers/HomeworkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MSIT155Site.Controllers
+{
+    public class HomeworkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
